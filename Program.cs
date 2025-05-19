b3c2e81e9c10a004e70a12be3bfb2962e25360d2
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2025-05-19 06:56:45 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//*****************************************************************************
+//** 3024. Type of Triangle                                         leetcode **
+//*****************************************************************************
+
+char* triangleType(int* nums, int numsSize) {
+    int a, b, c;
+    a = nums[0];
+    b = nums[1];
+    c = nums[2];
+
+    // Triangle inequality and non-positive side check
+    if (a <= 0 || b <= 0 || c <= 0 ||
+        a + b <= c || a + c <= b || b + c <= a)
+    {
+        return "none";
+    }
+
+    // Check triangle type
+    if (a == b && b == c)
+    {
+        return "equilateral";
+    }
+    else if (a == b || b == c || c == a)
+    {
+        return "isosceles";
+    }
+    else
+    {
+        return "scalene";
+    }
+}
